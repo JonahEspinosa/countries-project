9b0b02c0c7f84302f683e0811a2884d0cbf5c729
Jonah <espinosajonah1245@gmail.com>
2022-09-20 12:50:58 -0400

    submt

--- country.js
+++ country.js
@@ -27,15 +27,6 @@ const countries = [
 
 
 
-
-
-
-
-
-
-
-
-
 document.addEventListener('DOMContentLoaded',function(){
     
     let searchBtn = document.getElementById("search-btn");
@@ -57,9 +48,13 @@ allbtn.addEventListener("click",()=>{
     searchBtn.addEventListener("submit",async(event)=> {
         event.preventDefault()
         let countryName = countryInp.value;
-    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    
+    
+        let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
     console.log(finalURL);
-fetch(finalURL).then((response) => response.json()).then((data) => { 
+
+
+    fetch(finalURL).then((response) => response.json()).then((data) => { 
     console.log(data[0]);
 console.log(data[0].capital[0]);
 console.log(data[0].flags.svg);
@@ -69,6 +64,9 @@ console.log(Object.keys(data[0].currencies)[0]);
 console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
 console.log(Object.values(data[0].languages).toString().split(",").join(",")
 );
+
+
+
 result.innerHTML = `
 <img src="${data[0].flags.svg}" class="flag-img">
 <h2>${data[0].name.common}</h2>
